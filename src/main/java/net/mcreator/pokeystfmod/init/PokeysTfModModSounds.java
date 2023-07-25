6e06b3708aa278dbee366f330bb7c2717c75acbd
pokeyt59 <Tobias@DESKTOP-5T4G8J3>
2023-07-25 09:15:30 -0500

    v0.0.0 pre launch v0.0.1 (just getting the basics set up)

---
+++ src/main/java/net/mcreator/pokeystfmod/init/PokeysTfModModSounds.java
@@ -0,0 +1,20 @@
+
+/*
+ *    MCreator note: This file will be REGENERATED on each build.
+ */
+package net.mcreator.pokeystfmod.init;
+
+import net.minecraftforge.registries.RegistryObject;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.DeferredRegister;
+
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.resources.ResourceLocation;
+
+import net.mcreator.pokeystfmod.PokeysTfModMod;
+
+public class PokeysTfModModSounds {
+	public static final DeferredRegister<SoundEvent> REGISTRY = DeferredRegister.create(ForgeRegistries.SOUND_EVENTS, PokeysTfModMod.MODID);
+	public static final RegistryObject<SoundEvent> MEDIC_HURT_SOUNDS = REGISTRY.register("medic_hurt_sounds", () -> SoundEvent.createVariableRangeEvent(new ResourceLocation("pokeys_tf_mod", "medic_hurt_sounds")));
+	public static final RegistryObject<SoundEvent> MEDIC_DEATH_SOUNDS = REGISTRY.register("medic_death_sounds", () -> SoundEvent.createVariableRangeEvent(new ResourceLocation("pokeys_tf_mod", "medic_death_sounds")));
+}
